4ebcdf64b391a62ce26d410f610b3c420d27a9e4
RafieSA <rafiesafaraz@student.telkomuniversity.ac.id>
2025-11-23 14:14:27 +0700

    done: chapter komentar

---
+++ bin/comment.dart
@@ -0,0 +1,13 @@
+/// This is main function
+/// Will executed by Dart.
+void main (){
+
+  //This is 'name' variable.
+  var name = 'Rafie Safaraz Aribowo';
+
+  /**
+  * This is multi-line comments.
+  * You can add more, here.
+  */
+  print (name);
+}
--- bin/variable.dart
+++ bin/variable.dart
@@ -1,4 +1,5 @@
 void main() {
+  // This is variable.
   var name = 'Rafie';
 
   print(name);
