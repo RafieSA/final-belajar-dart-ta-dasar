6e5f27091a3324db46c92d654032d84c930321c6
RafieSA <rafiesafaraz@student.telkomuniversity.ac.id>
2025-11-23 11:09:13 +0700

    done: dart create

---
+++ bin/final_belajar_dart_ta.dart
@@ -0,0 +1,5 @@
+import 'package:final_belajar_dart_ta/final_belajar_dart_ta.dart' as final_belajar_dart_ta;
+
+void main(List<String> arguments) {
+  print('Hello world: ${final_belajar_dart_ta.calculate()}!');
+}
---
+++ test/final_belajar_dart_ta_test.dart
@@ -0,0 +1,8 @@
+import 'package:final_belajar_dart_ta/final_belajar_dart_ta.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
