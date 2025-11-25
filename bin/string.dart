af4713e2551e7327e4df96af3cd149c892fa7e47
RafieSA <rafiesafaraz@student.telkomuniversity.ac.id>
2025-11-25 17:48:07 +0700

    done: string interpolation

--- bin/string.dart
+++ bin/string.dart
@@ -1,9 +1,12 @@
 void main() {
 
   String firstName = 'Rafie';
-
   String lastName = "Aribowo";
 
   print(firstName);
   print(lastName);
+
+  var fullName = '$firstName ${lastName}';
+  print(fullName);
+
 }
