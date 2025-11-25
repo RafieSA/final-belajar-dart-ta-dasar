0debbfef94acf4a0940206ca70736c8f11ae73e2
RafieSA <rafiesafaraz@student.telkomuniversity.ac.id>
2025-11-25 18:01:37 +0700

    done: chapter multiline string

--- bin/string.dart
+++ bin/string.dart
@@ -15,10 +15,18 @@ void main() {
   print(text);
 
   // chapter menggabungkan string
-  var name1 = firstName + " " +lastName;
+  var name1 = firstName + " " + lastName;
   var name2 = 'Rafie' ' Safaraz' ' Aribowo';
 
   print(name1);
   print(name2);
 
+  // chapter multiline string
+  var longString = '''
+this is long string
+multiline string
+learning dart
+  ''';
+
+  print(longString);
 }
