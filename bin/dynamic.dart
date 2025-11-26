6a1bc236b6429c29fb1edb8ac744ff044122b2e0
RafieSA <rafiesafaraz@student.telkomuniversity.ac.id>
2025-11-26 08:47:31 +0700

    done: chapter dynamic & konversi tipe data

---
+++ bin/dynamic.dart
@@ -0,0 +1,16 @@
+void main() {
+
+  var contoh; // otomatis menjadi dynamic
+  var contoh1 = 100; // otomatis menjadi int
+  contoh = 100;
+
+  dynamic variable = 100;
+  print(variable);
+
+  variable = true;
+  print(variable);
+
+  variable = 'Rafie';
+  print(variable);
+
+}
