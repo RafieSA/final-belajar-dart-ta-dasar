a23b5a8b1ae35347f011692820eb4e1b691fc12a
RafieSA <rafiesafaraz@student.telkomuniversity.ac.id>
2025-11-26 08:51:54 +0700

    done: operator aritmatika

---
+++ bin/operator_aritmatika.dart
@@ -0,0 +1,13 @@
+void main() {
+
+  var first = 10;
+  var second = 3;
+
+  print(first + second);
+  print(first - second);
+  print(first * second);
+  print(first / second); // hasilnya double
+  print(first ~/ second); // hasilnya int
+  print(first % second); // sisa bagi (modulo)
+
+}
